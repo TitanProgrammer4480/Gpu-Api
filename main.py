bdf09f7d38972dfb236074baa280a21a9e2510c7
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-10-23 09:42:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from functions.web_scraping_GPU import get_price
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello"
+    return render_template("home.html")
 
 @app.route("/api/gpu/price/<gpu>")
 def gpu_price(gpu):
