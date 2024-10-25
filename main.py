7de91dba406cdbb3c3ae67cc5367be4b8467bb16
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-10-25 10:49:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from functions.web_scraping_GPU import get_price
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"]) 
 def home():
+    if request.method == "POST":
+        search_term = request.form["search_term"]
+        results = get_price(search_term)
+        return render_template("home.html", results=results)
     return render_template("home.html")
 
 @app.route("/api/gpu/price/<gpu>")
